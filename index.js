c5fd15d0486c6dab9607ef3ab642f1756162b641
Гафур <49081172+ilfae@users.noreply.github.com>
2025-07-13 21:56:59 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,12 @@ const app = express();
 const keys = require('./keys.json');
 let currentIndex = 0;
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  next();
-});
-
 app.get('/', (req, res) => {
   const key = keys[currentIndex];
   currentIndex = (currentIndex + 1) % keys.length;
   
-  res.send(`<!-- ${key} -->`);
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(key);
 });
 
 const PORT = process.env.PORT || 3000;
